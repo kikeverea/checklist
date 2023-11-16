1883b696ddf146912cde05e7cedc14fdf7f800af
Marcos Sanz <sanzmarcos18@gmail.com>
2023-11-16 15:17:34 +0100

    Show tasks in pending/competed sectioned list

--- checklist/checklist_frontend/src/views/checklist/TaskItem.js
+++ checklist/checklist_frontend/src/views/checklist/TaskItem.js
@@ -2,9 +2,22 @@ import { Text, StyleSheet, Pressable } from 'react-native'
 import BouncyCheckbox from 'react-native-bouncy-checkbox'
 import { colors } from '../../styles/styles'
 
-const TaskItem = ({ task, onCompletedChange, editing, inList, addToEdit, removeFromEdit }) => {
+const TaskItem = ({ task, completedStateChange, editing, inList, addToEdit, removeFromEdit }) => {
 
   const backgroundColor = inList ? colors.primaryLight : colors.defaultBackground
+  const checkboxColor = task.completed ? colors.primaryLight : colors.primary
+  const paddingLeft =  task.completed ? 32 : 16
+
+  const styles = StyleSheet.create({
+    taskStyle: {
+      flexDirection: 'row',
+      paddingLeft,
+      paddingTop: 16,
+      paddingRight: 48,
+      paddingBottom: 16,
+      backgroundColor
+    }
+  })
 
   const handlePress = () => {
     if (editing)
@@ -19,23 +32,12 @@ const TaskItem = ({ task, onCompletedChange, editing, inList, addToEdit, removeF
         addToEdit(task)
   }
 
-  const styles = StyleSheet.create({
-    taskStyle: {
-      flexDirection: 'row',
-      paddingLeft: 16,
-      paddingTop: 16,
-      paddingRight: 48,
-      paddingBottom: 16,
-      backgroundColor
-    }
-  })
-
   return (
     <Pressable style={ styles.taskStyle } onPress={ () => handlePress() } onLongPress={ () => handleEdit() }>
       <BouncyCheckbox
         isChecked={ task.completed }
-        onPress={() => { onCompletedChange(task, !task.completed) }}
-        fillColor={ colors.primary }
+        onPress={() => { completedStateChange(task, !task.completed) }}
+        fillColor={ checkboxColor }
         unfillColor="#FFFFFF"
         iconStyle={{ borderColor: colors.primary }}
       />
--- checklist/checklist_frontend/src/views/checklist/TaskList.js
+++ checklist/checklist_frontend/src/views/checklist/TaskList.js
@@ -1,26 +1,71 @@
-import { FlatList, View } from "react-native"
-import TaskItem from "./TaskItem"
+import { View, Text } from 'react-native'
+import TaskItem from './TaskItem'
+import { StyleSheet } from 'react-native'
+import { SectionList } from 'react-native'
+import { colors } from '../../styles/styles'
 
 const TaskList = ({ tasks, onTaskCompletedChange, selectionList }) => {
+
+  const COMPLETED_TASKS_TITLE = 'Completadas'
+  const NO_TITLE = 'no_title'
+
+  const styles = StyleSheet.create({
+    separator: {
+      height: 3
+    },
+    header: {
+      padding: 8,
+      backgroundColor: colors.primaryLight
+    },
+    title: {
+      color: colors.icons,
+      fontWeight: 'bold'
+    }
+  })
+
+  const renderItemSeparator = () => <View style={ styles.separator }/>
 
   const renderTaskItem = ({ item }) =>
     <TaskItem
       task={ item }
-      onCompletedChange={ onTaskCompletedChange }
+      completedStateChange={ onTaskCompletedChange }
       editing={ selectionList.length() > 0 }
       inList={ selectionList.inList(item) }
       addToEdit={ selectionList.add }
       removeFromEdit={ selectionList.remove }
     />
+
+  const sectioned = tasks.reduce(
+    (result, task) => {
+      const sectionIndex = task.completed ? 1 : 0
+      result[sectionIndex].data = result[sectionIndex].data
+        .concat(task)
+        .sort((task1, task2) => task1.id - task2.id)
+      return result
+    },
+    [{ title: NO_TITLE, data: []}, { title: COMPLETED_TASKS_TITLE, data: []}]
+  )
+
+  const renderSectionHeader = title =>
+    <>
+      {
+        title !== NO_TITLE && title === COMPLETED_TASKS_TITLE && sectioned[1].data.length > 0 &&
+        <View style={ styles.header }>
+          <Text style={ styles.title }>{ title }</Text>
+        </View>
+      }
+    </>
   
   return (
     <View>
-      <FlatList
+      <SectionList
         renderItem={ renderTaskItem }
-        data={ tasks.sort((task1, task2) => task1.id - task2.id) }
+        sections={ sectioned }
         keyExtractor={(task) => task.id}
         contentContainerStyle={{ paddingBottom: 60 }}
-      />
+        renderSectionHeader={({section: {title}}) => renderSectionHeader(title)}
+        ItemSeparatorComponent={ renderItemSeparator }
+      />      
     </View>
   )
 }
