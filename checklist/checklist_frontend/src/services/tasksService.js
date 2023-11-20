942a8bf50a6d9465bfa0bad4ad0cd2533fa8a432
Marcos Sanz <sanzmarcos18@gmail.com>
2023-11-20 14:42:46 +0100

    Bring back url parse function

--- checklist/checklist_frontend/src/services/tasksService.js
+++ checklist/checklist_frontend/src/services/tasksService.js
@@ -1,14 +1,21 @@
 import axios from "axios"
 
-const url = 'http://192.168.64.5:3000/tasks'
+const url = id => {
+  let url = 'http://192.168.64.5:3000/tasks'
+
+  if (id)
+    url += '/' + id
+
+  return url + '.json'
+}
 
 const getAll = async () => {
-    const res =  await axios.get(url)
+    const res =  await axios.get(url())
     return res.data
 }
 
 const createTask = async (user, description) => {
-    const res = await axios.post(url, { created_by: user.id, description })
+    const res = await axios.post(url(), { created_by: user.id, description })
     return res.data
 }
 
@@ -21,12 +28,12 @@ const updateTaskDescription = async (user, task, description) => {
 }
 
 const updateTask = async (task, updateContent) => {
-  const res = await axios.put(`url/${task.id}`, updateContent)
+  const res = await axios.put(url(task.id), updateContent)
   return res.data
 }
 
 const deleteTask = async id => {
-  const res = await axios.delete(url([id]))
+  const res = await axios.delete(url(id))
   return res.status === 204
 }
 
